13e14186068de52091e47c4f574e4d8f45a2513a
Artem Innokentiev <artinnok@mail.ru>
2016-05-26 23:35:36 +0300

    refactoring

--- late-bot.py
+++ late-bot.py
@@ -21,7 +21,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def connect_close(fn):
+# database connect and close
+def db_connect_close(fn):
     def wrap(*args, **kwargs):
         db.connect()
         fn(*args, **kwargs)
@@ -29,10 +30,27 @@ def connect_close(fn):
     return wrap
 
 
+# checks
+def is_delay_correct(text):
+    if re.search('[^0-9](?u)', text):
+        return False
+    else:
+        return True
+
+
+def is_reason_correct(text):
+    if re.search('[^a-zA-Zа-яА-Я](?u)', text):
+        return False
+    else:
+        return True
+
+
+# db manipulations
 def get_or_create_person(message):
-    username = message.to_dict().get('from').get('username')
-    first_name = message.to_dict().get('from').get('first_name')
-    last_name = message.to_dict().get('from').get('last_name')
+    message_from = message.to_dict().get('from')
+    username = message_from.get('username')
+    first_name = message_from.get('first_name')
+    last_name = message_from.get('last_name')
     person, created = Person.get_or_create(
         username=username,
         defaults={
@@ -43,94 +61,48 @@ def get_or_create_person(message):
     return person
 
 
-def enter_delay(bot, update):
-    bot.sendMessage(
-        update.message.chat_id,
-        text='Введите время задержки в минутах'
-    )
-
-
-def is_delay_correct(text):
-    if re.search('[^0-9](?u)', text):
-        return False
-    else:
-        return True
-
-
 def create_delay(person, minute):
     Delay.create(person=person, minute=minute)
 
 
-def enter_reason(bot, update):
-    bot.sendMessage(
-        update.message.chat_id,
-        text='Введите причину задержки'
-    )
-
-
-def retry_enter_delay(bot, update):
-    bot.sendMessage(
-        update.message.chat_id,
-        text='Введите целое число - минуты задержки'
-    )
-
-
-def is_reason_correct(text):
-    if re.search('[^a-zA-Zа-яА-Я](?u)', text):
-        return False
-    else:
-        return True
-
-
-def create_reason(username, text):
-    person = Person.get(username=username)
+def create_reason(person, text):
     last_delay = list(person.delays)[-1]
     last_delay.reason = text
     last_delay.save()
 
 
-def finish(bot, update):
-    bot.sendMessage(
-        update.message.chat_id,
-        text='Успех!'
-    )
-
-
-def retry_enter_reason(bot, update):
-    bot.sendMessage(
-        update.message.chat_id,
-        text='Введите словесную причину, без цифр и остальных символов'
-    )
-
-
-@connect_close
+@db_connect_close
 def main_handler(bot, update):
     person = get_or_create_person(update.message)
-    username = update.message.to_dict().get('from').get('username')
+    username = person.username
     user_state = state.get(username, MENU)
     text = update.message.text
+    chat_id = update.message.chat_id
 
-    if user_state == MENU:
-        enter_delay(bot, update)
+    if user_state == MENU and text[0] == '/':
+        bot.sendMessage(chat_id, 'Введите время задержки в минутах')
         state[username] = ENTER_DELAY
+
     elif user_state == ENTER_DELAY:
         if is_delay_correct(text):
             minute = int(text)
             create_delay(person, minute)
-            enter_reason(bot, update)
+            bot.sendMessage(chat_id, 'Введите причину задержки')
             state[username] = ENTER_REASON
         else:
-            retry_enter_delay(bot, update)
+            bot.sendMessage(chat_id, 'Введите целое число - минуты задержки')
 
     elif user_state == ENTER_REASON:
         if is_reason_correct(text):
-            create_reason(username, text)
-            finish(bot, update)
+            create_reason(person, text)
+            bot.sendMessage(chat_id, 'Успех!')
             state[username] = MENU
         else:
-            retry_enter_reason(bot, update)
+            bot.sendMessage(chat_id, 'Введите словесную причину, без цифр и '
+                                     'остальных символов')
 
 
+# debug function
 def get_upd(bot, update):
     print(update.message)
 
