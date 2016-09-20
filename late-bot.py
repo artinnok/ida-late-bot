caca05f75915fa7befd56f5740144ff92933f9f2
Artem Innokentiev <artinnok@mail.ru>
2016-09-20 13:55:54 +0300

    REFACTOR

--- late-bot.py
+++ late-bot.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 import logging
-
 import os
 import re
-import telegram
-from orm.models import db, Delay, Person, fn
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from orm.models import db, Delay, Person, fn
 
 # const
 API_TOKEN = os.environ['API_TOKEN']
 MENU, ENTER_DELAY, ENTER_REASON = range(3)
-
-# global vars
-state = {}
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -31,21 +28,6 @@ def db_connect_close(func):
     return wrap
 
 
-# checks
-def is_delay_correct(text):
-    if re.search('[^0-9](?u)', text):
-        return False
-    else:
-        return True
-
-
-def is_reason_correct(text):
-    if re.search('[^a-zA-Zа-яА-Я](?u)', text):
-        return False
-    else:
-        return True
-
-
 # db manipulations
 def get_or_create_person(message):
     message_from = message.to_dict().get('from')
@@ -62,48 +44,21 @@ def get_or_create_person(message):
     return person
 
 
-def create_delay(person, minute):
-    Delay.create(person=person, minute=minute)
-
-
-def create_reason(person, text):
-    last_delay = list(person.delays)[-1]
-    last_delay.reason = text
-    last_delay.save()
+def extract_delay_num(text):
+    
 
 
 @db_connect_close
 def main_handler(bot, update):
     person = get_or_create_person(update.message)
-    username = person.username
-    user_state = state.get(username, MENU)
     text = update.message.text
-    chat_id = update.message.chat_id
 
-    if user_state == MENU and re.search('([ао]п[ао][зс]д|задерж)(?iu)', text):
-        bot.sendMessage(chat_id, 'Используйте команду /late')
-
-    elif user_state == MENU and text[0] == '/':
-        bot.sendMessage(chat_id, 'Введите время задержки в минутах')
-        state[username] = ENTER_DELAY
-
-    elif user_state == ENTER_DELAY:
-        if is_delay_correct(text):
-            minute = int(text)
-            create_delay(person, minute)
-            bot.sendMessage(chat_id, 'Введите причину задержки')
-            state[username] = ENTER_REASON
-        else:
-            bot.sendMessage(chat_id, 'Введите целое число - минуты задержки')
-
-    elif user_state == ENTER_REASON:
-        if is_reason_correct(text):
-            create_reason(person, text)
-            bot.sendMessage(chat_id, 'Успех!')
-            state[username] = MENU
-        else:
-            bot.sendMessage(chat_id, 'Введите словесную причину, без цифр и '
-                                     'остальных символов')
+    if re.search('([ао]п[ао][зс]д|задерж)(?iu)', text):
+        num = extract_delay_num(text)
+        type = extract_delay_type()
+        minutes = convert_num(num, type)
+        reason = extract_reason()
+        save_delay(user, minutes, reason)
 
 
 # show users delay stats
@@ -127,7 +82,7 @@ def show_stats(bot, update):
 
 
 # debug function
-def get_upd(bot, update):
+def debug_update(bot, update):
     print(update.message)
 
 
@@ -142,11 +97,9 @@ def main():
     dp.add_handler(CommandHandler("late", main_handler))
     dp.add_handler(CommandHandler("stats", show_stats))
     dp.add_handler(MessageHandler([Filters.text], main_handler))
-
     dp.add_error_handler(error)
 
     updater.start_polling()
-
     updater.idle()
 
 if __name__ == '__main__':
